156456f4109f1488099d02dbacd11ca372b997c8
rhian <opalla.rhianmae@gmail.com>
2026-02-17 19:55:59 +0800

    Week2

---
+++ week2.py
@@ -0,0 +1,16 @@
+name = "Rhian Mae M. Opalla"
+age = 18
+height = float(1.50)
+working = False
+
+print("Full Name:", name)
+print(type(name))
+print("")
+print("Age:", 18)
+print(type(age))
+print("")
+print("Height:", height)
+print(type(height))
+print("")
+print("Is working?", working)
+print(type(working))
